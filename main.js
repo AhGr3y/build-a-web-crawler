975ddbce47087046e2421bb250608e223209e9a6
ahgr3y <leehh97@gmail.com>
2024-05-13 17:20:33 +0800

    Added validation for no. of args passed to main

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+import { argv } from 'node:process';
+
+function main() {
+    if (argv.length !== 3) {
+        console.log('Invalid number of argument(s), please input only ONE argument.')
+        return
+    }
+    console.log(`Crawling thru "${argv[2]}" at the moment...`)
+}
+
+main()
